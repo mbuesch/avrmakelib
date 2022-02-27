6a3bbe97f0fb98272a5b3e33b066e1e4325b00b4
Michael Buesch <m@bues.ch>
2022-02-27 16:43:01 +0100

    Add helper script to install all dependencies

---
+++ install-dependencies-deb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Install build dependencies on a Debian based operating system.
+#
+
+apt install \
+	avr-libc \
+	avrdude \
+	binutils-avr \
+	doxygen \
+	gcc-avr \
+	make \
+	python3 \
+	sparse
